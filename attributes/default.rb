465b6c96586f016afad0d4a4b5c9b3f64b7f466a
David Amian <damian@emergya.com>
2012-01-30 18:13:32 +0100

    Fix problem with default arguments

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 # Groups list definition
 default[:group_management][:admin_groups]['0'][:group] = 'admin'
 default[:group_management][:admin_groups]['1'][:group] = 'adm'
-default[:group_management][:admin_users_to_add] = []
-default[:group_management][:admin_users_to_remove] = [] 
+default[:group_management][:admin_users_to_add] = {}
+default[:group_management][:admin_users_to_remove] = {}
 default[:group_management][:base_groups]['0'][:group] = 'dialout'
 default[:group_management][:base_groups]['1'][:group] = 'fax'
 default[:group_management][:base_groups]['2'][:group] = 'cdrom'
@@ -14,5 +14,5 @@ default[:group_management][:base_groups]['7'][:group] = 'video'
 default[:group_management][:base_groups]['8'][:group] = 'plugdev'
 default[:group_management][:base_groups]['9'][:group] = 'fuse'
 default[:group_management][:base_groups]['10'][:group] = 'sambashare'
-default[:group_management][:base_users_to_add] = []
-default[:group_management][:base_users_to_remove] = []
+default[:group_management][:base_users_to_add] = {}
+default[:group_management][:base_users_to_remove] = {}
